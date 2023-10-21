3fc9fdf7a6b49690e4212a09e2f43cc30cd5972d
stnDinus <111202214433@mhs.dinus.ac.id>
2023-10-21 17:38:56 +0700

    feat: working calculator

---
+++ lib/calculator.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+
+enum Symbol { operand, operator }
+
+enum Operator { bagi, kali, kurangi, tambah }
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  double hasil = 0;
+  double? lhs;
+  Operator? operator;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      CalculatorDisplay(displayValue: hasil),
+      CalculatorButtons(
+        callback: (Symbol symbol, Object value) {
+          switch (symbol) {
+            case Symbol.operator:
+              operator = value as Operator;
+              break;
+            case Symbol.operand:
+              if (lhs == null) {
+                lhs = value as double;
+              } else {
+                switch (operator!) {
+                  case Operator.bagi:
+                    setState(() {
+                      hasil = lhs! / (value as double);
+                    });
+                    break;
+                  case Operator.kali:
+                    setState(() {
+                      hasil = lhs! * (value as double);
+                    });
+                    break;
+                  case Operator.kurangi:
+                    setState(() {
+                      hasil = lhs! - (value as double);
+                    });
+                    break;
+                  case Operator.tambah:
+                    setState(() {
+                      hasil = lhs! + (value as double);
+                    });
+                    break;
+                }
+              }
+              break;
+          }
+        },
+      )
+    ]);
+  }
+}
+
+class CalculatorDisplay extends StatefulWidget {
+  final double displayValue;
+  const CalculatorDisplay({super.key, required this.displayValue});
+
+  @override
+  State<StatefulWidget> createState() => _CalculatorDisplayState();
+}
+
+class _CalculatorDisplayState extends State<CalculatorDisplay> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(widget.displayValue.toString());
+  }
+}
+
+class CalculatorButtons extends StatelessWidget {
+  final Function(Symbol symbol, Object value) callback;
+
+  const CalculatorButtons({super.key, required this.callback});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 4,
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      children: [
+        CalculatorButton(
+            string: "7",
+            onPressed: () {
+              callback(Symbol.operand, 7.0);
+            }),
+        CalculatorButton(
+            string: "8",
+            onPressed: () {
+              callback(Symbol.operand, 8.0);
+            }),
+        CalculatorButton(
+            string: "9",
+            onPressed: () {
+              callback(Symbol.operand, 9.0);
+            }),
+        CalculatorButton(
+            string: "/",
+            onPressed: () {
+              callback(Symbol.operator, Operator.bagi);
+            }),
+        CalculatorButton(
+            string: "4",
+            onPressed: () {
+              callback(Symbol.operand, 4.0);
+            }),
+        CalculatorButton(
+            string: "5",
+            onPressed: () {
+              callback(Symbol.operand, 5.0);
+            }),
+        CalculatorButton(
+            string: "6",
+            onPressed: () {
+              callback(Symbol.operand, 6.0);
+            }),
+        CalculatorButton(
+            string: "x",
+            onPressed: () {
+              callback(Symbol.operator, Operator.kali);
+            }),
+        CalculatorButton(
+            string: "1",
+            onPressed: () {
+              callback(Symbol.operand, 1.0);
+            }),
+        CalculatorButton(
+            string: "2",
+            onPressed: () {
+              callback(Symbol.operand, 2.0);
+            }),
+        CalculatorButton(
+            string: "3",
+            onPressed: () {
+              callback(Symbol.operand, 3.0);
+            }),
+        CalculatorButton(
+            string: "-",
+            onPressed: () {
+              callback(Symbol.operator, Operator.kurangi);
+            }),
+        const SizedBox.shrink(),
+        CalculatorButton(
+            string: "0",
+            onPressed: () {
+              callback(Symbol.operand, 0.0);
+            }),
+        const SizedBox.shrink(),
+        CalculatorButton(
+            string: "+",
+            onPressed: () {
+              callback(Symbol.operator, Operator.tambah);
+            }),
+      ],
+    );
+  }
+}
+
+class CalculatorButton extends StatelessWidget {
+  final String string;
+  final Function() onPressed;
+
+  const CalculatorButton(
+      {super.key, required this.string, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: onPressed, child: Text(string));
+  }
+}
